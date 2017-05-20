923fd8ac32e60626ad0b8e4877472292f348aa40
lassana <lassana.dansoko@gmail.com>
2017-05-20 15:25:02 +0200

    rien

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 ﻿<?php
 try
 {
-$bdd = new PDO('mysql:host=db680011459.db.1and1.com;dbname=db680011459;charset=utf8', 'dbo680011459', 'nx78azmtM%');
+    $bdd = new PDO('mysql:host=localhost;dbname=it-les', 'root', '');
 }
 catch (Exception $e)
 {
