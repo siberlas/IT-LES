f2f4fe78509926b29227a0c756bbafeaaccd95c4
siberlas <lassana.dansoko@gmail.com>
2017-05-27 14:38:43 +0200

    page deboucher et menu

--- actionv2.php
+++ actionv2.php
@@ -1,8 +1,8 @@
 <?php
 try
 {
-   //$bdd = new PDO('mysql:host=localhost;dbname=it-les', 'root', '');
-    $bdd = new PDO('mysql:host=itlesfrazuadm.mysql.db;dbname=itlesfrazuadm', 'itlesfrazuadm', 'nx78azmtM');
+   $bdd = new PDO('mysql:host=localhost;dbname=it-les', 'root', '');
+    //$bdd = new PDO('mysql:host=itlesfrazuadm.mysql.db;dbname=itlesfrazuadm', 'itlesfrazuadm', 'nx78azmtM');
 }
 catch (Exception $e)
 {
--- connection.php
+++ connection.php
@@ -1,7 +1,9 @@
 ﻿<?php
 try
 {
-    $bdd = new PDO('mysql:host=itlesfrazuadm.mysql.db;dbname=itlesfrazuadm', 'itlesfrazuadm', 'nx78azmtM');
+    //$bdd = new PDO('mysql:host=itlesfrazuadm.mysql.db;dbname=itlesfrazuadm', 'itlesfrazuadm', 'nx78azmtM');
+    $bdd = new PDO('mysql:host=localhost;dbname=it-les', 'root', '');
+
 }
 catch (Exception $e)
 {
